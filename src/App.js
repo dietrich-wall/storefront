72f81e28b1281e7715b91ae82bbc13127c4e10aa
dietrich-wall <dietrich.wall@pm.me>
2023-05-15 01:25:45 +0200

    nav-to-all-routes

--- src/App.js
+++ src/App.js
@@ -1,15 +1,30 @@
 import './App.css';
+import React, { useState } from 'react';
 import Home from './pages/index';
 import SigninPage from './pages/signin';
+import Sidebar from './components/Sidebar';
+import Navbar from './components/Navbar';
+
 import {
   BrowserRouter,
   Routes,
   Route,
 } from "react-router-dom";
 
+
 function App() {
+
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggle = () => {
+    setIsOpen(!isOpen);
+  };
+
+
   return (
     <BrowserRouter>
+      <Sidebar isOpen={isOpen} toggle={toggle} />
+      <Navbar toggle={toggle} />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path='/signin' element={<SigninPage />} exact />
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,29 +1,20 @@
-import React, { useState } from 'react';
+// import React, { useState } from 'react';
 import CoverSection from '../components/CoverSection';
-import Footer from '../components/Footer';
 import InfoSection from '../components/InfoSection';
 import { aboutObj, discoverObj, signupObj } from '../components/InfoSection/Data';
-import Navbar from '../components/Navbar';
+// import Navbar from '../components/Navbar';
 import Services from '../components/Services';
-import Sidebar from '../components/Sidebar';
+// import Sidebar from '../components/Sidebar';
 
 const Home = () => {
-  const [isOpen, setIsOpen] = useState(false);
-
-  const toggle = () => {
-    setIsOpen(!isOpen);
-  };
 
   return (
     <>
-      <Sidebar isOpen={isOpen} toggle={toggle} />
-      <Navbar toggle={toggle} />
       <CoverSection />
       <InfoSection {...aboutObj} />
       <InfoSection {...discoverObj} />
       <InfoSection {...signupObj} />
       <Services />
-      <Footer />
     </>
   );
 };
